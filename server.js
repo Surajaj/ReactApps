90aac87cc465246a71b1d29eb5488f06f1f0c1ae
Suraj A J <surajaj100@gmail.com>
2017-03-26 15:32:51 +0530

    prep repo for heroku

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ var express = require('express');
 // Create our app
 var app = express();
 
+// Middleware
+app.use(function(req, res, next) {
+	if (req.headers['x-forward-proto'] === 'http') {
+		next();
+	} else {
+		res.redirect("http://" + req.hostname + req.url);
+	}
+});
+
 // Server statics
 app.use(express.static('public'));
 
+// Port
+const PORT = process.env.PORT || 3000;
+
 // Listen
-app.listen(3000, function() {
-	console.log("Express running at 3000");
+app.listen(PORT, function() {
+	console.log("Express running at " + PORT);
 });
