9a34bc1ef28f05fc36612c0d453afd3f00f1a19d
Suraj A J <surajaj100@gmail.com>
2017-03-26 15:50:35 +0530

    redirect

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ var http = require('http');
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
